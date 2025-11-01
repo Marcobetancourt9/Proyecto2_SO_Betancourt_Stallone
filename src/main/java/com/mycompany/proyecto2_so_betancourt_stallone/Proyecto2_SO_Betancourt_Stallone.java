bbc4d0e891a600087a0592fd8dc1a468aeb08929
Marcobetancourt9 <marco.betancourt@correo.unimet.edu.ve>
2025-10-31 23:37:59 -0400

    commit

---
+++ src/main/java/com/mycompany/proyecto2_so_betancourt_stallone/Proyecto2_SO_Betancourt_Stallone.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.proyecto2_so_betancourt_stallone;
+
+/**
+ *
+ * @author marco
+ */
+public class Proyecto2_SO_Betancourt_Stallone {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
